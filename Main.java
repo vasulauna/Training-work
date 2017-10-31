db13034c007a77daefa691654daf1585dff24eec
vasulauna <33039366+vasulauna@users.noreply.github.com>
2017-10-31 10:26:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
-package com.companu;
+package com.company;
 import java.io.IOException;
 import java.until.scanner;
 public class Main{
 public static void main(String[] args) throws IOException {
 int x ;
 int y ;
-Scanner reader = new scanner(system.in);
+Scanner reader = new Scanner(System.in);
 System.out.println("Enter x: ");
 x = reader.nextInt();
 System.out.println("Enter y: ");
 y = reader.nextInt();
 System.out.println("Answer");
 System.out.println(x + y);
-reader.closw();
+reader.close();
 }
 }

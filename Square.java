ebdd8c43b056a26d70c022db36cc5c17227b8743
vasulauna <33039366+vasulauna@users.noreply.github.com>
2017-11-08 23:12:48 +0300

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,11 +1,28 @@
- class Square extends Figure {
-    int area;
-    
-  
-    Figure(int a, int b) {
-        height = a;
-        length = b;
-     
-        
-    }
+ class  Figure {
+	private int length; 
+	private int height;
+	
+	Figure(Figure my) { // передача объекта конструктору
+		length = my.length;
+		height = my.height;
+	}
+
+	
+	Figure(int 6, int 5,) {
+		length = 6;
+		height = 5;
+	}
+
+	}
+}
+
+class Rectangle extends Figure{
+	Rectangle(int area) {
+		
+	int getVolume() {
+      area = Length * height;
+      System.out.println("area =");
+		
+	}
+}
 }
